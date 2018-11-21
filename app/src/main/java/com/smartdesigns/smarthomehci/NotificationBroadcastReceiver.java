8409f0839ea979267fe380994c7045b20a1230e9
pvedoya <pvedoya@itba.edu.ar>
2018-11-20 21:48:16 -0300

    broadcast reciever

--- app/src/main/java/com/smartdesigns/smarthomehci/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/smartdesigns/smarthomehci/NotificationBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.smartdesigns.smarthomehci;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -7,15 +8,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.TaskStackBuilder;
 
 public class NotificationBroadcastReceiver extends BroadcastReceiver {
     public static final String ACTION_NEW_MESSAGE = "com.smartdesigns.smarthomehci.ACTION_NEW_MESSAGE";
+    private int counter = 0;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        int messageId = intent.getIntExtra(Devices.MESSAGE_ID, 0);
+        int messageId = intent.getIntExtra("id",0);
 
         Intent notificationIntent = new Intent(context, Devices.class);
 
@@ -26,12 +30,15 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
         final PendingIntent contentIntent =
                 stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Notification notification = new Notification.Builder(context)
-                .setContentTitle("Blinds has changed status")
-                .setContentText("Blinds are up")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        @SuppressLint("ResourceAsColor") Notification notification = new Notification.Builder(context)
+                .setContentTitle(intent.getStringExtra("title"))
+                .setContentText(intent.getStringExtra("subtitle"))
+                .setSmallIcon(R.drawable.ic_smarthome)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), intent.getIntExtra("image", R.drawable.notfound)))
+                .setColor(R.color.blue)
                 .setAutoCancel(true)
+                .setSound(defaultSoundUri)
                 .setContentIntent(contentIntent)
                 .build();
 
