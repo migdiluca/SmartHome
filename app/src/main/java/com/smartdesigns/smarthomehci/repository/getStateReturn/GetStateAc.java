ace1732f600a92e5ee856e967f8438f1ab86ecb7
premigio <premigio@itba.edu.ar>
2018-11-22 03:07:26 -0300

    f

--- app/src/main/java/com/smartdesigns/smarthomehci/repository/getStateReturn/GetStateAc.java
+++ app/src/main/java/com/smartdesigns/smarthomehci/repository/getStateReturn/GetStateAc.java
@@ -1,4 +1,4 @@
-package com.smartdesigns.smarthomehci.repository.GetStateReturn;
+package com.smartdesigns.smarthomehci.repository.getStateReturn;
 
 public class GetStateAc {
     private String status;
@@ -6,14 +6,16 @@ public class GetStateAc {
     private String verticalSwing;
     private String horizontalSwing;
     private String fanSpeed;
+    private String mode;
 
     public GetStateAc(String status, int temperature, String verticalSwing, String horizontalSwing,
-                      String fanSpeed){
+                      String fanSpeed, String mode){
         this.status = status;
         this.temperature = temperature;
         this.verticalSwing = verticalSwing;
         this.horizontalSwing = horizontalSwing;
         this.fanSpeed = fanSpeed;
+        this.mode = mode;
     }
 
     public String getStatus() {
@@ -34,5 +36,9 @@ public class GetStateAc {
 
     public String getHorizontalSwing() {
         return horizontalSwing;
+    }
+
+    public String getMode() {
+        return mode;
     }
 }
--- app/src/main/java/com/smartdesigns/smarthomehci/repository/getStateReturn/GetStateBlinds.java
+++ app/src/main/java/com/smartdesigns/smarthomehci/repository/getStateReturn/GetStateBlinds.java
@@ -1,4 +1,4 @@
-package com.smartdesigns.smarthomehci.repository.GetStateReturn;
+package com.smartdesigns.smarthomehci.repository.getStateReturn;
 
 public class GetStateBlinds {
     private String status;
